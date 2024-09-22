40b12bde94963a4cfc1810fdde41e8d53c05c2d6
Miguel Ferrer <miguelff222@gmail.com>
2024-09-22 21:15:43 +0200

    Refactor logging functions to include newline character at the end of each message

--- logs.php
+++ logs.php
@@ -2,25 +2,25 @@
 
 function log_error($message)
 {
-    echo "\033[31m[ERROR] $message\033[0m"; // Rojo
+    echo "\033[31m[ERROR] $message\033[0m\n"; // Rojo
 }
 
 function log_warning($message)
 {
-    echo "\033[33m[WARNING] $message\033[0m"; // Amarillo
+    echo "\033[33m[WARNING] $message\033[0m\n"; // Amarillo
 }
 
 function log_info($message)
 {
-    echo "\033[32m[INFO] $message\033[0m"; // Verde
+    echo "\033[32m[INFO] $message\033[0m\n"; // Verde
 }
 
 function log_debug($message)
 {
-    echo "\033[34m[DEBUG] $message\033[0m"; // Azul
+    echo "\033[34m[DEBUG] $message\033[0m\n"; // Azul
 }
 
 function log_verbose($message)
 {
-    echo "\033[37m[VERBOSE] $message\033[0m"; // Blanco
+    echo "\033[37m[VERBOSE] $message\033[0m\n"; // Blanco
 }
