babb0254aa814000276b747d50ef1fae5c418b20
Miguel Ferrer <miguelff222@gmail.com>
2024-09-22 21:16:38 +0200

    Refactor user.php to create and connect a client socket to the server

--- user.php
+++ user.php
@@ -10,5 +10,21 @@ $server_port = 8888;
 $download_directory = "users/" . basename(__DIR__) . "/download/";
 $shared_directory = "users/" . basename(__DIR__) . "/shared/";
 
-echo $download_directory;
-echo $shared_directory;
+// crear el socket del cliente en la $ip y $puerto
+$sock = socket_create(AF_INET, SOCK_STREAM, getprotobyname('tcp'));
+if ($sock === false) {
+    die("Error creating the socket: " . socket_strerror(socket_last_error()));
+} else {
+    echo "Socket created\n";
+}
+
+if (socket_bind($sock, $ip, $port) === false) {
+    die("Error al asociar el socket: " . socket_strerror(socket_last_error($sock)));
+}
+
+// conectar al servidor central
+if (socket_connect($sock, $server_ip, $server_port) === false) {
+    die("Error connecting to the server: " . socket_strerror(socket_last_error($sock)));
+} else {
+    echo "Connected to the server\n";
+}
