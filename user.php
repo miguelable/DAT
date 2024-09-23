abb4dffb071114489d693f1ad94fdc046c2167ed
Miguel Ferrer <miguelff222@gmail.com>
2024-09-23 16:09:52 +0200

    Refactor user.php to send shared files to the server in the background

--- user.php
+++ user.php
@@ -28,3 +28,44 @@ if (socket_connect($sock, $server_ip, $server_port) === false) {
 } else {
     echo "Connected to the server\n";
 }
+
+function send_shared_files($shared_directory, $ip, $sock, $server_ip, $server_port)
+{
+    // get files from the shared folder
+    $files = scandir($shared_directory);
+    foreach ($files as $file) {
+        if ($file == "." || $file == "..") {
+            continue;
+        }
+        $shared_files[] = $file;
+    }
+
+    // get json object of $shared files with name files
+    $shared_files = ["files" => $shared_files];
+    $json_shared_files = json_encode($shared_files);
+
+    // send the file to the server
+    $request = "PUT /hosts/$ip HTTP/1.1\r\n
+                Host: $server_ip:$server_port\r\n
+                Content-Type: application/json\r\n
+                Content-Length: " . strlen($json_shared_files) . "\r\n\r\n
+                $json_shared_files";
+    socket_write($sock, $request, strlen($request));
+    echo $request;
+}
+
+
+// hilo para enviar los ficheros en segundo plano cada 10 seg
+$pid = pcntl_fork();
+if ($pid == -1) {
+    die("Error forking...\n");
+} else if ($pid == 0) {
+    while (true) {
+        send_shared_files($shared_directory, $ip, $sock, $server_ip, $server_port);
+        sleep(10);
+    }
+} else {
+    // leer la respuesta del servidor
+    $response = socket_read($sock, 1024);
+    echo $response;
+}
