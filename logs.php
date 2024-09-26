68177499e326fbe23f31b4e9c596f1ff54eae741
MiguelFerrerF <mferrer@inbiot.es>
2024-09-26 16:00:27 -0600

    Refactorizar funciones de registro de logs para verificar si LOG_ON está definido antes de hacer el log

--- logs.php
+++ logs.php
@@ -1,26 +1,32 @@
 <?php
 
+// Verifica si LOG_ON está definido antes de hacer el log
 function log_error($message)
 {
-    echo "\033[31m[ERROR] $message\033[0m\n"; // Rojo
+    if (defined('LOG_ON') && LOG_ON)
+        echo "\033[31m[ERROR] $message\033[0m\n"; // Rojo
 }
 
 function log_warning($message)
 {
-    echo "\033[33m[WARNING] $message\033[0m\n"; // Amarillo
+    if (defined('LOG_ON') && LOG_ON)
+        echo "\033[33m[WARNING] $message\033[0m\n"; // Amarillo
 }
 
 function log_info($message)
 {
-    echo "\033[32m[INFO] $message\033[0m\n"; // Verde
+    if (defined('LOG_ON') && LOG_ON)
+        echo "\033[32m[INFO] $message\033[0m\n"; // Verde
 }
 
 function log_debug($message)
 {
-    echo "\033[34m[DEBUG] $message\033[0m\n"; // Azul
+    if (defined('LOG_ON') && LOG_ON)
+        echo "\033[34m[DEBUG] $message\033[0m\n"; // Azul
 }
 
 function log_verbose($message)
 {
-    echo "\033[37m[VERBOSE] $message\033[0m\n"; // Blanco
+    if (defined('LOG_ON') && LOG_ON)
+        echo "\033[37m[VERBOSE] $message\033[0m\n"; // Blanco
 }
