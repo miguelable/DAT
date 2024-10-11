be708b3c3180b63e6d04a6b0eaebec57595d65a5
paulafernandeztorres <paufeto20@gmail.com>
2024-10-11 11:03:10 +0200

    añadir humedad

--- Sensor/enviar_datos.sh
+++ Sensor/enviar_datos.sh
@@ -10,9 +10,10 @@ while true
 do
     # Datos de la sonda
     TEMPERATURA=$(awk -v min=0 -v max=50 'BEGIN{srand(); printf "%.1f", min+rand()*(max-min)}')
+    HUMEDAD=$(awk -v min=0 -v max=100 'BEGIN{srand(); printf "%.1f", min+rand()*(max-min)}')
     FECHA=$(date +"%Y-%m-%dT%H:%M:%S")
     # Construir el JSON
-    JSON="{\"ID\": $ID, \"temperatura\": $TEMPERATURA, \"timestamp\": \"$FECHA\"}"
+    JSON="{\"ID\": $ID, \"temperatura\": $TEMPERATURA, \"humedad\": $HUMEDAD, \"timestamp\": \"$FECHA\"}"
     # Enviar datos al gateway
     curl -X POST http://localhost:8080/gateway.php -H "Content-Type: application/json" -d "$JSON"
     sleep 10
