0f3af89eb43683458491da1cf31555337f87368b
MiguelFerrerF <mferrer@inbiot.es>
2024-11-12 02:25:08 +0100

    Actualizar script para enviar datos: incluir potencia, estado del LED y manejar respuesta del gateway

--- Sensor/enviar_datos.sh
+++ Sensor/enviar_datos.sh
@@ -1,22 +1,3 @@
-# 
-# Este script envía datos de temperatura y humedad simulados a un gateway en formato JSON cada 10 segundos.
-# 
-# Dependencias:
-# - bc: Para realizar cálculos de punto flotante.
-# - curl: Para enviar solicitudes HTTP.
-# 
-# Funciones:
-# - command_exists: Verifica si un comando está disponible en el sistema.
-# 
-# Flujo del script:
-# 1. Verifica si las dependencias (bc, curl) están instaladas. Si no, las instala usando apt-get.
-# 2. Genera un ID de sensor.
-# 3. En un bucle infinito, genera valores aleatorios de temperatura (0-50) y humedad (0-100) con un decimal.
-# 4. Construye un objeto JSON con el ID del sensor, temperatura, humedad y la marca de tiempo actual.
-# 5. Envía el objeto JSON al gateway usando curl.
-# 6. Espera 10 segundos antes de repetir el proceso.
-
-
 #!/bin/bash
 
 # Función para verificar si un comando está disponible
@@ -38,6 +19,7 @@ if ! command_exists curl; then
 fi
 
 ID=48
+ESTADO_LED=1
 
 # Enviar de forma periódica datos de temperatura aleatorios entre 0 y 50 cada 10 segundos con decimal
 # Se envían al gateway en formato JSON
@@ -47,21 +29,24 @@ echo "Simulando envio de datos cada 10 segundos"
 # Datos de la sonda
 while true
 do
-    # Generar temperatura aleatoria entre 0 y 50 con un decimal
-    TEMPERATURA=$(printf "%.1f" "$(echo "scale=1; $RANDOM/3276.7" | bc)")
-    # Generar humedad aleatoria entre 0 y 100 con un decimal
-    HUMEDAD=$(printf "%.1f" "$(echo "scale=1; $RANDOM/327.67" | bc)")
+    # Genera un valor de potencia aleatorio entre 0 y 4095
+    POTENCIA=$(shuf -i 0-4095 -n 1)
     FECHA=$(date +"%Y-%m-%dT%H:%M:%S")
     # Construir el JSON
-    JSON="{\"ID\": $ID, \"temperatura\": $TEMPERATURA, \"humedad\": $HUMEDAD, \"timestamp\": \"$FECHA\"}"
-    # Enviar datos al gateway
-    curl -X POST http://localhost:8081/gateway.php -H "Content-Type: application/json" -d "$JSON"
-    
-    if [ $? -ne 0 ]; then
-        echo "Error al enviar los datos."
-    else
-        echo "Datos enviados: $JSON"
+    JSON="{\"id_sonda\": $ID, \"potencia\": $POTENCIA, \"timestamp\": \"$FECHA\", \"actual_status\": $ESTADO_LED}"
+    # Enviar datos al gateway y esperar respuesta de desired led state
+    respuesta=$(curl -s -X POST -H "Content-Type: application/json" -d "$JSON" http://localhost:8081/gateway.php)
+    # Datos enviados
+    echo "Datos enviados: $JSON"
+    # Extraer desired_status de la respuesta
+    desired_status=$(echo $respuesta | jq -r '.desired_status')
+
+    # Comparar desired_status con ESTADO_LED
+    if [ "$desired_status" != "$ESTADO_LED" ]; then
+        echo "Cambiando estado del LED de $ESTADO_LED a $desired_status"
+        ESTADO_LED=$desired_status
     fi
+
     # Esperar 10 segundos   
     sleep 10
 done
