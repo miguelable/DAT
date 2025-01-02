93dcf0394f9892c8cf0543df9f94b4538d41da60
miguelable <miguelff222@gmail.com>
2025-01-03 00:53:07 +0100

    Agregar funcionalidad para activar un LED y una cámara con un sensor HCSR04, incluyendo múltiples efectos de LED en el bucle principal.

--- TecnoPark/src/main.cpp
+++ TecnoPark/src/main.cpp
@@ -1,18 +1,88 @@
+#include "LedEffects.h"
+#include "hcsr04.h"
 #include <Arduino.h>
 
-#define LED_PIN 4
+#define FLASH_PIN 2
+#define PIXEL_COUNT 1
+#define LED_PIN 13
+#define TRIG_PIN 14
+#define ECHO_PIN 15
 
-void setup() {
-  Serial.begin(115200); // UART1 para logs
-  Serial.println("Iniciando logs en UART1");
-  pinMode(LED_PIN, OUTPUT);
-  Serial.println("Iniciando LED");
+HCSR04 sensor(TRIG_PIN, ECHO_PIN);
+
+void activationCallback()
+{
+  // Aquí se encenderá el led y la cámara
+  Serial.println("Encendiendo Led");
+  digitalWrite(FLASH_PIN, HIGH);
+  Serial.println("Encendiendo Camara");
+  delay(1000);
+  digitalWrite(FLASH_PIN, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+void setup()
+{
+  Serial.begin(115200); // UART1 para logs
+  sensor.setCallback(15, activationCallback);
+  sensor.init();
+  pinMode(FLASH_PIN, OUTPUT);
+  setupLed();
+  Serial.println("Setup done");
+}
+
+void loop()
+{
+  static uint8_t effectIndex = 8;
+
+  // Ciclo a través de los diferentes efectos
+  switch (effectIndex) {
+    case 0:
+      Serial.println("Effect: None");
+      setLedEffect(None);
+      break;
+    case 1:
+      Serial.println("Effect: Flashing");
+      setLedEffect(Flashing);
+      break;
+    case 2:
+      Serial.println("Effect: Processing");
+      setLedEffect(Processing);
+      break;
+    case 3:
+      Serial.println("Effect: Success");
+      setLedEffect(Success);
+      break;
+    case 4:
+      Serial.println("Effect: Error");
+      setLedEffect(Error);
+      break;
+    case 5:
+      Serial.println("Effect: Warning");
+      setLedEffect(Warning);
+      break;
+    case 6:
+      Serial.println("Effect: Closed");
+      setLedEffect(Closed);
+      break;
+    case 7:
+      Serial.println("Effect: Opened");
+      setLedEffect(Opened);
+      break;
+    case 8:
+      Serial.println("Effect: Waiting");
+      setLedEffect(Waiting);
+      break;
+    case 9:
+      Serial.println("Effect: RevisionNeeded");
+      setLedEffect(RevisionNeeded);
+      break;
+    default:
+      Serial.println("Effect: Cleared");
+      setLedEffect(Clear);
+      effectIndex = 0;
+      return;
+  }
+
+  effectIndex++;
+  delay(10000); // Cambiar de efecto cada 5 segundos
 }
